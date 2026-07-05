687c4b7c7195882a427b91ec0dc4a22fd74b2a95
harikahono <kahonokun@gmail.com>
2026-07-05 19:23:43 +0700

    fix(event): ShouldBroadcastNow bypasses queue serialization

--- seiris-be/app/Events/EquityUpdated.php
+++ seiris-be/app/Events/EquityUpdated.php
@@ -7,11 +7,11 @@ use App\Models\EquitySnapshot;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class EquityUpdated implements ShouldBroadcast
+class EquityUpdated implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
