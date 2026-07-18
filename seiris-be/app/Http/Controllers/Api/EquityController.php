487c42daaa1b354c14bbc33649f192616e448b2a
harikahono <kahonokun@gmail.com>
2026-07-19 01:44:37 +0700

    fix: tambah use DB facade di EquityController
    Class DB gak ditemuin pas project-scoped export
    karena pake DB::table() tanpa import use statement.
    Hanya ngaruh di block if(\) yg dieksekusi
    pas route project-scoped (bukan team-scoped).

--- seiris-be/app/Http/Controllers/Api/EquityController.php
+++ seiris-be/app/Http/Controllers/Api/EquityController.php
@@ -10,6 +10,7 @@ use App\Models\Team;
 use App\Models\Project;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
